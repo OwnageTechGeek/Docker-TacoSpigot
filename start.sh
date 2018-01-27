279b9a51ead9c89cc840dacd9622031e78862a42
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2018-01-27 11:48:19 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 chown -R tacospigot /$PROCESSHOME/
 cd /$PROCESSHOME/
-su - waterfall -c 'java -Xms$XMS -Xmx$XMX -Dcom.mojang.eula.agree=true -jar TacoSpigot.jar --world-dir worlds'
+su - tacospigot -c 'java -Xms$XMS -Xmx$XMX -Dcom.mojang.eula.agree=true -jar TacoSpigot.jar --world-dir worlds'
 bash
